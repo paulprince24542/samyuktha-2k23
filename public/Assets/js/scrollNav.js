7e55e7ce4176a6393b56b85078741450a80b9d42
Adithya-S-Nair <adithyasnair00@gmail.com>
2023-03-20 14:51:14 +0530

    navbar fixed

--- public/Assets/js/scrollNav.js
+++ public/Assets/js/scrollNav.js
@@ -2,12 +2,13 @@
 
 var body = document.body;
 var header = document.getElementById('header');
+var nav = document.querySelector('nav');
 let lastScroll = 0
 window.addEventListener('scroll', () => {
     const currentScroll = window.scrollY;
     if (currentScroll <= 0) {
         body.classList.remove("scroll-up")
-        header.style.background='transparent';
+        header.style.background = 'transparent';
     }
     if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
         body.classList.remove("scroll-up")
@@ -16,9 +17,16 @@ window.addEventListener('scroll', () => {
     if (currentScroll < lastScroll && body.classList.contains("scroll-down")) {
         body.classList.remove("scroll-down")
         body.classList.add("scroll-up")
-        header.style.background='#141414 ';
-        header.style.borderRadius='6em';
-        header.style.top='1em';
+        if (window.innerWidth > 700) {
+            header.style.background = '#141414 ';
+            header.style.borderRadius = '6em';
+            header.style.top = '1em';
+            header.style.marginTop='unset'
+            nav.style.margin='2.5em'
+        } else {
+            header.style.backdropFilter = "blur(5px)";
+            header.style.padding = "1em";
+        }
     }
     lastScroll = currentScroll
 })
